fd0da36ccbdcd41659a96641d6bafe378d4bb466
Carlos Henrique <ichfs9108@gmail.com>
2025-02-18 23:56:41 -0300

    feat: persistindo registros com o mapeamento @onetoone

---
+++ ecommerce/src/main/java/tech/buildrun/ecommerce/controller/UserController.java
@@ -0,0 +1,30 @@
+package tech.buildrun.ecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.buildrun.ecommerce.controller.dto.CreateUserDto;
+import tech.buildrun.ecommerce.service.UserService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDto dto) {
+
+        var user = userService.createUser(dto);
+
+        return ResponseEntity.created(URI.create("/users/" + user.getUserId())).build();
+    }
+}
---
+++ ecommerce/src/main/java/tech/buildrun/ecommerce/service/UserService.java
@@ -0,0 +1,37 @@
+package tech.buildrun.ecommerce.service;
+
+import org.springframework.stereotype.Service;
+import tech.buildrun.ecommerce.controller.dto.CreateUserDto;
+import tech.buildrun.ecommerce.entities.BillingAddressEntity;
+import tech.buildrun.ecommerce.entities.UserEntity;
+import tech.buildrun.ecommerce.repository.BillingAddressRepository;
+import tech.buildrun.ecommerce.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BillingAddressRepository billingAddressRepository;
+
+    public UserService(UserRepository userRepository,
+                       BillingAddressRepository billingAddressRepository) {
+        this.userRepository = userRepository;
+        this.billingAddressRepository = billingAddressRepository;
+    }
+
+    public UserEntity createUser(CreateUserDto dto) {
+
+        var billingAddress = new BillingAddressEntity();
+        billingAddress.setAddress(dto.address());
+        billingAddress.setNumber(dto.number());
+        billingAddress.setComplement(dto.complement());
+
+        var savedBillingAddress = billingAddressRepository.save(billingAddress);
+
+        var user = new UserEntity();
+        user.setFullName(dto.fullName());
+        user.setBillingAddress(savedBillingAddress);
+
+        return userRepository.save(user);
+    }
+}
